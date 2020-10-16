76eb876b77ebc6d68bc0de6403cd8a68b749b4de
positiveko <edie_ko@naver.com>
2020-10-16 18:31:11 +0900

    Edit JS

--- calculator_javascript/src/index.js
+++ calculator_javascript/src/index.js
@@ -1,92 +1,79 @@
-// // <⚠️ DONT DELETE THIS ⚠️>
-// import "./styles.css";
-// // <⚠️ /DONT DELETE THIS ⚠️>
-const calResult = document.querySelector('.calResult'),
-    cancelBtn = document.querySelector('.cancleBtn'),
-    equalBtn = document.querySelector('.equalBtn'),
-    numBtn = document.querySelectorAll('.numBtn'),
-    operatorBtn = document.querySelectorAll('.operatorBtn');
-let calculating,
-    operating,
-    isNewInput;
+
+const calResult = document.querySelector(".calResult");
+// let calculating, operating, isNewInput;
+let calculating = 0,
+operating = "",
+isNewInput = true;
 
 // numBtn 클릭하면 result에 뜨도록
 function inputNum(num) {
-    let numbers = calResult.innerText;
-    // if (numbers === '0') {
-    //     numbers = '';
-    // }
-    // if (operating !== null) {
-    //     numbers 
-    // }
-    if (isNewInput) {
-        numbers = '';
-    }
-    calResult.innerText = numbers + num;
-    isNewInput = false;
+  let numbers = calResult.innerText;
+  if (numbers === '0') {
+      numbers = '';
+  }
+  if (isNewInput) {
+    numbers = "";
+  }
+  calResult.innerText = numbers + num;
+  isNewInput = false;
 }
 
 // clear
 function clearResult() {
-    
-    calResult.innerText = '0';
-    isNewInput = true;
-    operating = '';
-    calculating = 0;
+  calResult.innerText = "0";
+  isNewInput = true;
+  operating = "";
+  calculating = 0;
 }
 
 // 연산자 이용 계산
 function calculate(operator) {
-    let nums = parseFloat(calResult.innerText);
-    if (operating == '') {
-        calculating = nums;
-    } else {
-        if (!isNewInput) {
-            if ( operating == '+') {
-                calculating += nums;
-            } else if(operating == '-') {
-                calculating -= nums;
-            } else if (operating == '*') {
-                calculating *= nums;
-            } else if (operating == '/') {
-                if (nums == '0') {return;}
-                calculating /= nums;
-            }
+  let nums = parseFloat(calResult.innerText);
+  if (operating == "") {
+    calculating = nums;
+  } else {
+    if (!isNewInput) {
+      if (operating == "+") {
+        calculating += nums;
+      } else if (operating == "-") {
+        calculating -= nums;
+      } else if (operating == "*") {
+        calculating *= nums;
+      } else if (operating == "/") {
+        if (nums == "0") {
+          return;
         }
+        calculating /= nums;
+      }
     }
-    calResult.innerText = calculating;
-    operating = operator;
-    isNewInput = true;
+  }
+  calResult.innerText = calculating;
+  operating = operator;
+  isNewInput = true;
 }
 
+// equalBtn 구현
 function equal() {
-    let nums = parseFloat(calResult.innerText);
-    if (operating == '') {
-        calculating = nums;
-    } else {
-        if (!isNewInput) {
-            if ( operating == '+') {
-                calculating += nums;
-            } else if(operating == '-') {
-                calculating -= nums;
-            } else if (operating == '*') {
-                calculating *= nums;
-            } else if (operating == '/') {
-                if (nums == '0') {return;}
-                calculating /= nums;
-            }
+  let nums = parseFloat(calResult.innerText);
+  if (operating == "") {
+    calculating = nums;
+  } else {
+    if (!isNewInput) {
+     if (operating == "+") {
+        calculating += nums;
+        } else if (operating == "-") {
+        calculating -= nums;
+      } else if (operating == "*") {
+        calculating *= nums;
+      } else if (operating == "/") {
+        if (nums == "0") {
+          return;
         }
+        calculating /= nums;
+      }
     }
-    calResult.innerText = calculating;
-    operating = '';
-    isNewInput = true;
+  }
+  calResult.innerText = calculating;
+  operating = "";
+  isNewInput = true;
 }
-
-function init() {
-    calculating = 0;
-    operating = '';
-    isNewInput = true;
-}
-
-init();
-
